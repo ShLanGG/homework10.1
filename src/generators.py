07721ca90b70d25295dd43d9dcb45fa69f04b32d
Dmitriy Avdeyev <dmitriy.avdeyev13@gmail.com>
2024-07-18 13:15:20 +0200

    add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -8,3 +8,8 @@ def transaction_descriptions(transactions):
     for description in transactions:
         yield description.get("description")
 
+
+def card_number_generator(start, end):
+    for num in range(start, end + 1):
+        card_number = f'{num:016d}'
+        yield f'{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:16]}'
